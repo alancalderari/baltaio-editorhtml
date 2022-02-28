f96c70d9595ad441380875bafb4a175345636371
ALAN CALDERARI <79069079+alancalderari@users.noreply.github.com>
2022-02-27 21:01:19 -0300

    Add files via upload

---
+++ EditorHTML/Editor.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+namespace EditorHTML{
+    public static class Editor
+    {
+        public static void Show() {
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
+            Console.WriteLine("MODO EDITOR");
+            Console.WriteLine("------------");
+
+            Start();
+        }
+
+        public static void Start() {
+            var file = new StringBuilder();
+
+            do
+            {
+                file.AppendLine(Console.ReadLine());
+                file.AppendLine(Environment.NewLine);
+            }while(Console.ReadKey().Key != ConsoleKey.Escape);
+
+            Console.WriteLine("------------");
+            Console.WriteLine("    Deseja Salvar o arquivo?");
+            Viewer.Show(file.ToString());
+
+        }
+    }
+
+}
